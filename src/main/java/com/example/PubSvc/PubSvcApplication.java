076bcc9d724f6ba5034dc5d5c4273c604fe230e2
Fan Liu <charles_liuf@hotmail.com>
2018-08-20 21:57:25 +0800

    init commit

---
+++ src/main/java/com/example/PubSvc/PubSvcApplication.java
@@ -0,0 +1,20 @@
+package com.example.PubSvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PubSvcApplication {
+
+	@RequestMapping("/")
+	public String hello() {
+	  return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubSvcApplication.class, args);
+	}
+}
